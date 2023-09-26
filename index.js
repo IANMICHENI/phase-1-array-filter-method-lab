469efab6964500a7b9b50231b31e89d42a9ae88a
ian <ianmwendamicheni@gmail.com>
2023-09-26 10:59:51 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+
 // Code your solution here
+
+
+function findMatching (array, drivers){
+
+    const driversMatching = array.filter((element) =>{ 
+    element.drivers === drivers
+    if (element === drivers){
+    return element
+    }else if (element.toLowerCase()=== drivers.toLowerCase()){
+    return element
+    } 
+}
+)
+return driversMatching
+}
+ 
+function fuzzyMatch(array,letters){
+    const matchingLetters = array.filter((element) => element.startsWith(letters) )
+    return matchingLetters
+     }
+function matchName (array,drivers){
+    const nameMatches= array.filter ((element) => element.name === drivers)
+    return nameMatches
+}
